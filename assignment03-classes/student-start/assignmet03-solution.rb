ca8fc042eb59d187b47edf68de3e1f222d9e9fd4
Felipe Villa <felipevillarus@gmail.com>
2016-05-11 20:51:19 +0300

    Задание 3 - OK

--- assignment03-classes/student-start/assignmet03-solution.rb
+++ assignment03-classes/student-start/assignmet03-solution.rb
@@ -1,26 +1,34 @@
 class Person
   #have a first_name and last_name attribute with public accessors
   #attr_accessor 
+  attr_accessor :first_name
+  attr_accessor :last_name
 
   #have a class attribute called `people` that holds an array of objects
   @@people = []
 
   #have an `initialize` method to initialize each instance
-  def initialize(x,y)#should take 2 parameters for first_name and last_name
+  def initialize(f_name,l_name)#should take 2 parameters for first_name and last_name
     #assign those parameters to instance variables
+   @first_name = f_name
+   @last_name = l_name
     #add the created instance (self) to people class variable
+   @@people.push self
   end
 
   #have a `search` method to locate all people with a matching `last_name`
   def self.search(last_name)
     #accept a `last_name` parameter
+   @l_name = last_name
     #search the `people` class attribute for instances with the same `last_name`
     #return a collection of matching instances
+   @@people.select {|f| f.last_name==@l_name}
   end
 
   #have a `to_s` method to return a formatted string of the person's name
   def to_s
     #return a formatted string as `first_name(space)last_name`
+   "#@first_name #@last_name"
   end
 end
 
