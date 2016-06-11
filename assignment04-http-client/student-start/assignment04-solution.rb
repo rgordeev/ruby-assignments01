03b6c9142f59a8163b78a8a5f90b22dd22e4d8f5
Igor <nemytyshew@yandex.ru>
2016-06-11 20:43:07 +0300

    update 4 task

--- assignment04-http-client/student-start/assignment04-solution.rb
+++ assignment04-http-client/student-start/assignment04-solution.rb
@@ -3,7 +3,8 @@ require 'httparty'
 class Recipe
 include HTTParty
 base_uri "http://food2fork.com/api"
-default_params key: ENV["FOOD2FORK_KEY"]="cea0e8809a79bbc38c2e1f521d117388"
+#default_params key: ENV["FOOD2FORK_KEY"]="cea0e8809a79bbc38c2e1f521d117388"
+default_params key: ENV["FOOD2FORK_KEY"]
 format :json
 def self.for (keyword)
 get("/search", query: {q: keyword})["recipes"]
