c778059e29bd3f4c5511f648573c00f7bd9dff65
NovikovaA <nstarn@mail.ru>
2016-06-14 02:52:33 -0700

    all tasks

--- assignment04-http-client/student-start/assignment04-solution.rb
+++ assignment04-http-client/student-start/assignment04-solution.rb
@@ -1,5 +1,11 @@
 require 'httparty'
 
 class Recipe
-
+  include HTTParty
+  default_params key:ENV["FOOD2FORK_KEY"]='19720ef88ee003bc08d43eab18c7b634' 
+  base_uri 'http://food2fork.com/api'
+  format :json
+  def self.for keyword
+    return get('/search', query: {q: keyword})["recipes"]
+  end
 end
