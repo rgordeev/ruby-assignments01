08672bfa5afa1a6e6d921575db9e3c0af63a6548
klik0121 <dru0121@gmail.com>
2016-06-14 20:24:46 +0000

    Added rq07 functional. Application works, test rq07 doesn't.

--- assignment05-todolist/todolists/app/controllers/sessions_controller.rb
+++ assignment05-todolist/todolists/app/controllers/sessions_controller.rb
@@ -6,14 +6,21 @@ class SessionsController < ApplicationController
 	user = User.find_by(username: params[:session][:username])
 	if user && user.authenticate(params[:session][:password])
 	 log_in user
+ 	 redirect_to "/list"
         else
-	 flash.now[:danger] = 'Invalid username or password'	
+	 flash.now[:danger] = 'Invalid username or password'
 	 render 'new'
 	end
   end
 
   def destroy
     log_out
-    redirect_to login
+    redirect_to login_path
+  end
+
+  def list
+	user = current_user
+	@list = TodoList.find_by(user_id: user[:id])
+	render 'list'
   end
 end
--- assignment05-todolist/todolists/config/routes.rb
+++ assignment05-todolist/todolists/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'login_path' => 'sessions#new'
+  get 'login' => 'sessions#new'
+  get 	'list'	=> 'sessions#list'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
-resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
