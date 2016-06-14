908e2bf8daffe12b9bd095631ba3aa8dc1d3b2f0
WarHero1 <misha-belov1234@mail.ru>
2016-06-15 00:24:10 +0300

    HomeWork

--- assignment04-http-client/student-start/assignment04-solution.rb
+++ assignment04-http-client/student-start/assignment04-solution.rb
@@ -1,5 +1,11 @@
 require 'httparty'
 
 class Recipe
-
+  include HTTParty 
+  base_uri "http://food2fork.com/api"
+  default_params key: ENV["FOOD2FORK_KEY"]='28f832005cd709eab10f1d3d9d9c53b8' 
+  format :json
+  def self.for (keyword)
+    get("/search", query: {q: keyword})["recipes"]
+  end
 end
